19b0184c78ad06dced6299d0067e6b7e2ca55d26
Alan Van <alan.van@newsperform.com.au>
2020-05-15 22:32:22 +1000

    add examples

--- app/src/main/java/com/example/betweenus/injection/AppModule.kt
+++ app/src/main/java/com/example/betweenus/injection/AppModule.kt
@@ -1,9 +1,26 @@
 package com.example.betweenus.injection
 
+import com.example.betweenus.injection.example.TestConcreteImpl
+import com.example.betweenus.injection.example.TestConcreteInput
+import com.example.betweenus.injection.example.TestInterface
+import com.example.betweenus.injection.example.TestInterfaceImpl
+import com.example.betweenus.user_account.login.LoginViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 object AppModule {
-    fun get() = module {
 
+    val TEST_STRING1 = named("test_string1")
+    val TEST_STRING2 = named("test_string2")
+
+    fun get() = module {
+        factory<TestInterface> { TestInterfaceImpl() }
+        viewModel { LoginViewModel() }
+        single(TEST_STRING1) { "this is test string 1" }
+        single(TEST_STRING2) { "this is test string 2" }
+
+        single { TestConcreteInput() }
+        factory { TestConcreteImpl(get()) }
     }
 }
